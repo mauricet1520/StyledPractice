9d4a1cecffae966147df4f01104e8349ef735ea4
Maurice Thomas <mauricet1520@gmail.com>
2020-07-03 10:59:03 -0400

    Removed urls

--- app/src/main/java/com/coolreecedev/styledpractice/util/Global.kt
+++ app/src/main/java/com/coolreecedev/styledpractice/util/Global.kt
@@ -1,6 +1,6 @@
 package com.coolreecedev.styledpractice.util
 
 const val LOG_TAG: String = "style-logging"
-const val ZIP_CODE_VALIDATOR_URL = "https://Maurice:Reece@styled-zipcode-validator.herokuapp.com"
+const val ZIP_CODE_VALIDATOR_URL = ""
 const val DATE_MANAGER_URL = "https://date-manager.herokuapp.com"
-const val LOCAL_HOST = "https://Maurice:Reece@localhost:8080"
+const val LOCAL_HOST = ""
